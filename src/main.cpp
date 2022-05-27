ece0e2047c6b8106add6a43c827404241afa6767
Atilli <xitxat@outlook.com>
2022-05-27 16:23:49 +0200

    sensor name print

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
 #include <Arduino.h>
-
-// Include the libraries we need
+//#include "defines.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into D3 on the NodeMCU
-#define ONE_WIRE_BUS D3
+#define DEBUG 1                           // debug printer
+
+
+
+
+#define ONE_WIRE_BUS D3                   // Data wire is plugged into D3 on the NodeMCU
 #define TEMPERATURE_PRECISION 9
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(ONE_WIRE_BUS);            //  communicate with any OneWire device
+DallasTemperature sensors(&oneWire);      // Pass our oneWire reference to Dallas Temperature.
+//   DeviceAddress insideThermometer, outsideThermometer; // arrays to hold device addresses
 
-// Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sensors(&oneWire);
+ DeviceAddress insideThermometer    = { 0x28, 0x1C, 0x0F, 0x95, 0xF0, 0x01, 0x3C, 0x63 };
+ DeviceAddress outsideThermometer   = { 0x28, 0x3B, 0x6B, 0x95, 0xF0, 0xFF, 0x3C, 0x1B };
 
-// arrays to hold device addresses
-DeviceAddress insideThermometer, outsideThermometer;
 
-// Assign address manually. The addresses below will beed to be changed
-// to valid device addresses on your bus. Device address can be retrieved
-// by using either oneWire.search(deviceAddress) or individually via
-// sensors.getAddress(deviceAddress, index)
-// DeviceAddress insideThermometer = { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 };
-// DeviceAddress outsideThermometer   = { 0x28, 0x3F, 0x1C, 0x31, 0x2, 0x0, 0x0, 0x2 };
+
 
         /* FUNCTIONS */
         // function to print a device address
@@ -40,15 +37,24 @@ void printAddress(DeviceAddress deviceAddress)
       }
   }
 }
+
+void printProbe(DeviceAddress deviceAddress, char* z)   //  includes name string
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print(z);
+  Serial.print(tempC);
+  Serial.println();
+}
+
+
 void setup(void)
 {
- 
-  // start serial port
-  Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.begin(9600);               //  9600 OneWire stable
+  delay (500);
+  debug("debug setup");
+  debugln("******************DEBUG printer is ON*****************");
 
-  // Start up the library
-  sensors.begin();
+  sensors.begin();                  // Start up the OneWire library
 
   // locate devices on the bus
   Serial.print("Locating devices...");
@@ -56,33 +62,6 @@ void setup(void)
   Serial.print(sensors.getDeviceCount(), DEC);
   Serial.println(" devices.");
 
-  // report parasite power requirements
-  Serial.print("Parasite power is: ");
-  if (sensors.isParasitePowerMode()) Serial.println("ON");
-  else Serial.println("OFF");
-
-  // Search for devices on the bus and assign based on an index. Ideally,
-  // you would do this to initially discover addresses on the bus and then
-  // use those addresses and manually assign them (see above) once you know
-  // the devices on your bus (and assuming they don't change).
-  //
-  // method 1: by index
-  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0");
-  if (!sensors.getAddress(outsideThermometer, 1)) Serial.println("Unable to find address for Device 1");
-
-  // method 2: search()
-  // search() looks for the next device. Returns 1 if a new address has been
-  // returned. A zero might mean that the bus is shorted, there are no devices,
-  // or you have already retrieved all of them. It might be a good idea to
-  // check the CRC to make sure you didn't get garbage. The order is
-  // deterministic. You will always get the same devices in the same order
-  //
-  // Must be called before search()
-  //oneWire.reset_search();
-  // assigns the first address found to insideThermometer
-  //if (!oneWire.search(insideThermometer)) Serial.println("Unable to find address for insideThermometer");
-  // assigns the seconds address found to outsideThermometer
-  //if (!oneWire.search(outsideThermometer)) Serial.println("Unable to find address for outsideThermometer");
 
   // show the addresses we found on the bus
   Serial.print("Device 0 Address: ");
@@ -114,8 +93,6 @@ void printTemperature(DeviceAddress deviceAddress)
   float tempC = sensors.getTempC(deviceAddress);
   Serial.print("Temp C: ");
   Serial.print(tempC);
-  Serial.print(" Temp F: ");
-  Serial.print(DallasTemperature::toFahrenheit(tempC));
 }
 
 // function to print a device's resolution
@@ -147,8 +124,17 @@ void loop(void)
   sensors.requestTemperatures();
   Serial.println("DONE");
 
+    debugln("******************DEBUG LOOP ON*****************");
+
+
   // print the device information
   printData(insideThermometer);
   printData(outsideThermometer);
+      debugln("***");
+  Serial.println("probe data");
+    Serial.println("");
+
+  printProbe(insideThermometer, "Water Probe Temp: ");
+
   delay(5500);
 }
