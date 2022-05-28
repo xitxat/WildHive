2d6958cc216be25bf46e952b519ed26424de6d67
Atilli <xitxat@outlook.com>
2022-05-28 20:03:15 +0200

    cleaned

--- src/DallasFunctions.h
+++ src/DallasFunctions.h
@@ -2,16 +2,14 @@
 #define DALLAS_FUNCTIONS_H
 
 void setupDallas();
+void printDualProbes();
+void sensorRequest();
 
 void printAddress(DeviceAddress deviceAddress);
 void printProbe(DeviceAddress deviceAddress, char* z);
 void printTemperature(DeviceAddress deviceAddress);
 void printResolution(DeviceAddress deviceAddress);
 void printData(DeviceAddress deviceAddress);
-void sensorRequest();
-
-void printDualProbes();
-
 
 #endif
 
